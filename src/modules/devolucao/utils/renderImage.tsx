52fc560468ff93ac3aa93a6515674ff554f236cb
carlos <robertorsilva.krlos@gmail.com>
2025-11-21 18:20:18 -0300

    add form

--- src/modules/devolucao/utils/renderImage.tsx
+++ src/modules/devolucao/utils/renderImage.tsx
@@ -1,5 +1,5 @@
 export const renderImagePreview = (base64String: string | null, alt: string) => {
-  if (!base64String) return null
+  if (!base64String) return <p className="text-sm text-gray-500">Nenhuma foto encontrada</p>
   return (
     <div className="mt-2 flex justify-center">
       <img 
